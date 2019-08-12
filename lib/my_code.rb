ccebd9ee792ec0fbfba3ac279bad544017677c39
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 18:22:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,9 @@ end
 def reduce(source_array, starting_point = 0)
   i = 0
   while i < source_array.length
-    
+    if source_array[i] == Integer
+      sum += yield()
+    end
     i += 1
   end
 end
