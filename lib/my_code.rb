fc8535f731d0931b9eac0107da7c6f1ebda0edd6
Avinash Sarguru <avinashsarguru@gmail.com>
2019-08-12 18:34:16 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i +=1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point = 0)
+  i = 0
+  sum = 0
+  while i < source_array.length
+    if source_array[i].is_a? Integer
+      sum += yield(source_array[i], starting_point)
+      if starting_point > 0
+        starting_point = 0
+      end
+    else
+      if !yield(source_array[i], starting_point)
+        sum = false
+      else
+        sum = true
+      end
+    end
+    i += 1
+  end
+  return sum
+end
+
